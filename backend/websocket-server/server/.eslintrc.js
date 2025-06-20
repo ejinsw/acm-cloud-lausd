70f245dbc1cf0693c355f48d8ca08e4193b87a7b
Elijah Won <ejinsw@gmail.com>
2025-06-20 14:22:19 -0500

    chore: add ws linting

---
+++ backend/websocket-server/server/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    // Prettier
+    'prettier/prettier': 'error',
+
+    // General rules
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-unused-vars': 'warn',
+  },
+  ignorePatterns: ['node_modules/', 'dist/', 'build/', 'coverage/', '*.min.js', '*.bundle.js'],
+};

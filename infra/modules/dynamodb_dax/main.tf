4adabf085c654cf3d14993bf73405317386ffec5
Sid Chatterjee <siddharthchatterjee05@gmail.com>
2025-11-03 19:07:42 -0800

    Added resources for chat room members, messages, and members

--- infra/modules/dynamodb_dax/main.tf
+++ infra/modules/dynamodb_dax/main.tf
@@ -45,6 +45,83 @@ resource "aws_iam_role_policy" "dax_dynamodb_access" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "dax_service_managed" {
+  role       = aws_iam_role.dax_service.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDAXServiceRolePolicy"
+}
+
+resource "aws_dynamodb_table" "chat_rooms" {
+  name         = "lausd-chat-rooms-${var.environment}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "roomId"
+
+  attribute {
+    name = "roomId"
+    type = "S"
+  }
+
+  tags = { Environment = var.environment }
+}
+
+resource "aws_dynamodb_table" "chat_room_members" {
+  name         = "lausd-chat-room-members-${var.environment}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "roomId"
+  range_key    = "userId"
+
+  attribute { 
+    name = "roomId"
+   type = "S" 
+   }
+  attribute { 
+    name = "userId"
+     type = "S" 
+  }
+
+  tags = { Environment = var.environment }
+}
+
+resource "aws_dynamodb_table" "chat_room_messages" {
+  name         = "lausd-chat-room-messages-${var.environment}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "roomId"
+  range_key    = "sentAt"
+
+  attribute { 
+  name = "roomId"
+   type = "S" 
+   }
+  attribute { 
+    name = "sentAt"
+   type = "N" 
+  }
+  attribute { 
+    name = "messageId"
+    type = "S" 
+  }
+
+  global_secondary_index {
+    name            = "messageId"
+    hash_key        = "messageId"
+    projection_type = "ALL"
+  }
+
+  tags = { Environment = var.environment }
+}
+
+resource "aws_dynamodb_table" "chat_user_sessions" {
+  name         = "lausd-chat-user-sessions-${var.environment}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+
+  attribute { 
+  name = "userId"
+   type = "S"
+  } 
+
+  tags = { Environment = var.environment }
+}
+
 resource "aws_dax_cluster" "bar" {
   cluster_name       = "lausd-dax-cluster"
   iam_role_arn       = aws_iam_role.dax_service.arn
